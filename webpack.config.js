8a499e3283adfa8f8c7fe943ca8448f5e8386625
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2020-04-21 14:44:29 -0500

    Setup devDependencies and /dist

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -44,7 +44,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true        // makes server send back index.html every time, to let react router determine what needs to be shown on the screen
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     }
 }
