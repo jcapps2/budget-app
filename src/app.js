ba9638e91b338b4dfad017f7ad7e0bf2459fa6ed
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2020-04-23 11:26:31 -0500

    Setup env variables

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css'    // All browsers now start from the same place style-wise
 import './styles/styles.scss'   // Styling React section in Mead's React course
+import './firebase/firebase'
+
 
 // creating redux store
 const store = configureStore()
