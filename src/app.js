001109e25a7e4445e0b1f73265f7d14b30445483
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2020-04-23 12:59:13 -0500

    Added set expenses

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
+import { startSetExpenses } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'    // All browsers now start from the same place style-wise
 import './styles/styles.scss'   // Styling React section in Mead's React course
 import './firebase/firebase'
@@ -17,4 +20,8 @@ const jsx = (
     </Provider>
 )
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'))
+})
