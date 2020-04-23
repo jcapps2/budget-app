fdde012ae53ca52202e109d7a9a8bf4917e9570f
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2020-04-23 11:39:00 -0500

    Updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Header = () => {
     return (
         <header>
-            <h1>Expensify</h1>
+            <h1>Expense Forge</h1>
             <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
             <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
         </header>
