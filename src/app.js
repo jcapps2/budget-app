6d186605abf1d30bb98540bbaf6798bde2b82d09
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2020-04-25 13:46:19 -0500

    Add styles

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'normalize.css/normalize.css'    // All browsers now start from the same 
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 import { firebase } from './firebase/firebase'
+import LoadingPage from './components/LoadingPage'
 
 
 // creating redux store
@@ -29,7 +30,7 @@ const renderApp = () => {
     }
 }
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+ReactDOM.render(<LoadingPage />, document.getElementById('app'))
 
 // runs when authentication state changes
 firebase.auth().onAuthStateChanged((user) => {
